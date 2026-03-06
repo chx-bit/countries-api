9c4503291f2dff3a8c6129ef11ac9a32d3e9587a
chx-bit <135306956+chx-bit@users.noreply.github.com>
2026-03-07 02:58:04 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import { countriesRouter } from "./routers/countriesRouter.js";
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+const isProd = process.env.NODE_ENV === "production";
 
-// Middleware API
 app.use("/countries", countriesRouter);
+app.use(express.static(path.join(__dirname, "../public")));
 
-// app.listen(3000);
-// app.use("/src", express.static("src"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+if (!isProd) {
+    app.listen(3000, () => {
+        console.log("Dev server running at http://localhost:3000");
+    });
+}
 
 export default app;
